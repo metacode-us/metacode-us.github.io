b9bc168ab3e5bba3721be4f4eed354c8d496255a
Mark Smith-Guerrero <msmithgu@gmail.com>
2019-04-25 22:16:21 -0400

    animate

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var card = document.getElementById('card');
 var canvas = document.createElement('canvas');
 var numDots = 9;
-var dotRadius = 10;
+var dotRadius = 3;
+var dotGrowth = 3;
 
+var start;
 var ctx;
 var dotSpace;
 
@@ -16,16 +18,33 @@ canvas.style.top = '-50px';
 
 card.prepend(canvas);
 
+function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    for (var i=0; i < numDots; i++) {
+        for (var j=0; j < numDots; j++) {
+            ctx.beginPath();
+            ctx.arc(
+                dotSpace*(.5 + i),
+                dotSpace*(.5 + j),
+                (dotRadius + Math.sin(i - progress/500))*dotGrowth,
+                0,
+                Math.PI * 2,
+                true);
+            ctx.fill();
+        }
+    }
+    //console.log(dotRadius + Math.round(Math.sin(progress%4)*5));
+
+    window.requestAnimationFrame(step);
+}
+
 if (canvas.getContext) {
     ctx = canvas.getContext('2d');
     ctx.fillStyle = '#666';
     dotSpace = canvas.width / numDots;
 
-    for (var i=0; i < numDots; i++) {
-        for (var j=0; j < numDots; j++) {
-            ctx.beginPath();
-            ctx.arc(dotSpace*(.5 + i), dotSpace*(.5 + j), dotRadius, 0, Math.PI * 2, true);
-            ctx.fill();
-        }
-    }
+    window.requestAnimationFrame(step);
 }
