bd6f717b95cdba73c66874f2835d0706f60cf0ea
Mark Smith-Guerrero <msmithgu@gmail.com>
2019-04-25 21:30:05 -0400

    Draw something in the canvas..

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var card = document.getElementById('card');
-
 var canvas = document.createElement('canvas');
+var numDots = 9;
+var dotRadius = 10;
+
+var ctx;
+var dotSpace;
+
 canvas.id = 'dotc';
 canvas.width = card.clientHeight + 100;
 canvas.height = canvas.width;
@@ -8,6 +13,19 @@ canvas.style.zIndex = -5;
 canvas.style.position = 'absolute';
 canvas.style.left = '-100px';
 canvas.style.top = '-50px';
-canvas.style.border = '10px solid blue';
 
 card.prepend(canvas);
+
+if (canvas.getContext) {
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#666';
+    dotSpace = canvas.width / numDots;
+
+    for (var i=0; i < numDots; i++) {
+        for (var j=0; j < numDots; j++) {
+            ctx.beginPath();
+            ctx.arc(dotSpace*(.5 + i), dotSpace*(.5 + j), dotRadius, 0, Math.PI * 2, true);
+            ctx.fill();
+        }
+    }
+}
